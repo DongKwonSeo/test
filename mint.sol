16641ef5589c121e04d1c00df85be4407304faa8
DongKwon <gun100491@gmail.com>
2022-06-24 15:05:19 +0900

    Token 수정

--- mint.sol
+++ mint.sol
@@ -1,14 +1,13 @@
 pragma solidity ^0.8.0;
 
-import "@openzepplelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 contract MinKiloMeterToken is ERC721Enumerable{
 
-    constructor() ERC721("KiloMeter","KM"){}
-    function MinKiloMeterToken() public{
+    constructor() ERC721("KillMeter","KM"){}
+    function minKiloMeterToken() public{
         uint256 KiloMeterTokenId = totalSupply() + 1;
-
         _mint(msg.sender,KiloMeterTokenId);
-
+        
     }
 }
